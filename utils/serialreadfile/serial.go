9dba0ad52072a158d2976fb1040ede6896cca918
Frank Moreno <frankmoreno1993@gmail.com>
2017-11-07 21:38:46 -0500

    adding a neccesary flush

--- utils/serialreadfile/serial.go
+++ utils/serialreadfile/serial.go
@@ -7,3 +7,13 @@ func (h *Handler) Rx(buf []byte) (int,error){
 func (h *Handler) Tx(buf []byte) (int,error){
   return h.Serial.Write(buf)
 }
+
+func (h *Handler) Flush() {
+  h.Serial.Flush()
+
+  temp := make([]byte,1)
+  n := 1 // initial value different to 0
+  for n > 0{
+    n,_ = h.Serial.Read(temp)
+  }
+}
